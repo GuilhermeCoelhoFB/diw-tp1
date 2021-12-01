2ff2af2ecca19e06860cfa520c7399ddf9a3191f
Guilherme Coelho <guilherme.coelhofb@gmail.com>
2021-12-01 14:19:55 -0300

    ajax request to personal info

---
+++ frontend/js/app.js
@@ -0,0 +1,57 @@
+var body = document.querySelector("body");
+body.addEventListener("load", loadPersonalInfo());
+
+function loadPersonalInfo() {
+  let req = new XMLHttpRequest();
+
+  req.onload = function () {
+    let data = JSON.parse(this.responseText);
+    let perfilSection = `
+                    <div>
+                    <img
+                        id="picGuilherme"
+                        src="${data.avatar_url}"
+                        alt="foto perfil"
+                    />
+                    <div class="personalContainer">
+                        <h2>${data.name} <br> (${data.login})</h2>
+                        <div id="personalTextContainer">
+                        <p>
+                            <strong>Sobre mim:</strong>
+                            ${data.bio}
+                        </p>
+                        <div class="socialTextContainer">
+                            <h3>Redes Sociais</h3>
+                            <div id="socialContainer">
+                            <a
+                                target="_blank"
+                                href="${data.html_url}"
+                                ><img
+                                class="socialImg"
+                                src="../img/github-svgrepo-com.svg"
+                                alt=""
+                            /></a>
+                            <a
+                                target="_blank"
+                                href="${data.blog}"
+                                ><img
+                                class="socialImg"
+                                src="../img/linkedin-svgrepo-com.svg"
+                                alt=""
+                            /></a>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                
+                    `;
+    document.getElementById("personal").innerHTML = perfilSection;
+  };
+
+  req.onerror = function () {
+    alert(`Erro na request, error: "  ${this.status} - ${this.statusText} `);
+  };
+  req.open("GET", "https://api.github.com/users/GuilhermeCoelhoFB");
+  req.send();
+}
